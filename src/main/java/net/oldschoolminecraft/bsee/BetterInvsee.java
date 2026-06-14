9edbfd68e22acd7fcf0393368c41c3f5530ee024
moderatorman <12193288+moderatorman@users.noreply.github.com>
2026-06-13 17:42:42 -0700

    Cleanup imports

--- src/main/java/net/oldschoolminecraft/bsee/BetterInvsee.java
+++ src/main/java/net/oldschoolminecraft/bsee/BetterInvsee.java
@@ -1,17 +1,11 @@
 package net.oldschoolminecraft.bsee;
 
-import net.minecraft.server.Container;
 import net.minecraft.server.EntityPlayer;
 import net.minecraft.server.InventoryPlayer;
-import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.craftbukkit.inventory.CraftInventoryPlayer;
-import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.HashMap;
 
 public class BetterInvsee extends JavaPlugin
 {
@@ -22,7 +16,6 @@ public class BetterInvsee extends JavaPlugin
         instance = this;
 
         getCommand("invsee").setExecutor(new InvseeCommand());
-        getServer().getPluginManager().registerEvents(new EventProcessor(), this);
 
         System.out.println("BetterInvsee enabled");
     }
--- src/main/java/net/oldschoolminecraft/bsee/InvseeCommand.java
+++ src/main/java/net/oldschoolminecraft/bsee/InvseeCommand.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class InvseeCommand implements CommandExecutor
